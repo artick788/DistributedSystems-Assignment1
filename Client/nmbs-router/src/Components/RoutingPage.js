481bff10c612ef92d5fa26b417e33ef556569dc7
artick788 <60552965+artick788@users.noreply.github.com>
2022-04-08 15:39:42 +0200

    began working on routing page

--- Client/nmbs-router/src/Components/RoutingPage.js
+++ Client/nmbs-router/src/Components/RoutingPage.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Sidebar from "./Utils/SideBar";
+import SelectStationBar from "./Utils/SelectStationBar";
 import axios from "axios";
 import {Button, Divider, List, ListItem, Typography} from "@material-ui/core";
+import "./RoutingPage.css"
 
 export default class RoutingPage extends React.Component{
     constructor(props) {
         super(props);
+
+        this.state = {
+            selectedFromStation: -1,
+            selectedToStation: -1
+        }
     }
 
     render() {
         return (
-            <Typography variant="h6">
-                Hello Routing
-            </Typography>
+            <div className={"Bars"}>
+                <div className={"FromBar"}>
+                    <SelectStationBar title={"From station"} selectedCallback={(selectedStation) => {
+
+                    }}/>
+                </div>
+                <div className={"ToBar"}>
+                    <SelectStationBar title={"To station"} selectedCallback={(selectedStation) => {
+                        console.log(selectedStation);
+                    }}/>
+                </div>
+            </div>
         )
     }
 }
---
+++ Client/nmbs-router/src/Components/Utils/SelectStationBar.js
@@ -0,0 +1,67 @@
+import React from "react";
+import axios from "axios";
+import "./SelectStationBar.css"
+import store from "../Store/Store";
+import {getStationInfo} from "../Station";
+import {Button, Divider, List, ListItem, Typography} from "@material-ui/core";
+
+export default class SelectStationBar extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedStation: -1
+        }
+    }
+
+
+    render() {
+        if (store.getState().stations.length === 0){
+            getStationInfo();
+            return null;
+        }
+        else{
+            return (
+                <div className={"BarStyle"}>
+                    <List>
+                        <ListItem>
+                            <Typography variant="h6">
+                                {this.props.title}
+                            </Typography>
+                        </ListItem>
+                        <ListItem>
+                            <Typography variant="h6" style={{color: "#000000"}}>
+                                {store.getState().stationNames[this.state.selectedStation]}
+                            </Typography>
+                        </ListItem>
+                        <Divider/>
+                    </List>
+                    <div className={"ScrollBarStyle"}>
+                        <List>
+                            {
+                                store.getState().stations.map(
+                                    station => {
+                                        const stName = station.m_Name;
+                                        return (
+                                            <ListItem>
+                                                <Button className="ButtonStyle" onClick={() => {
+                                                    this.setState({...this.state, selectedStation: station.m_ElementID})
+                                                    this.props.selectedCallback(this.state.selectedStation);
+                                                }}>
+                                                    {
+                                                        stName
+                                                    }
+                                                </Button>
+                                            </ListItem>
+                                        )
+                                    }
+                                )
+                            }
+                        </List>
+                    </div>
+
+                </div>
+            )
+        }
+    }
+}
